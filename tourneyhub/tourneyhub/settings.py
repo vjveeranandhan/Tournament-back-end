78eaa33a06d6d60a6d877588eb74384fcd315b5d
vjveeranandhan <vjveeranandhan@gmail.com>
2024-07-21 22:23:35 +0200

    allowed host changed

--- tourneyhub/tourneyhub/settings.py
+++ tourneyhub/tourneyhub/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0s(bt9edp)f7aha@wwah@x-$)imx8vg1#z^#l^=ir&$dwg^zqt
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # settings.py
 
